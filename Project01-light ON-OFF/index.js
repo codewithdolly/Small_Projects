bac59bb6335f783c704139bcf65e24f95a3cde52
hicoderindia <hicoder.dolly@gmail.com>
2021-09-25 22:03:20 +0530

    Initial commit

---
+++ Project01-light ON-OFF/index.js
@@ -0,0 +1,13 @@
+//Light on
+function lightOn() {
+  document.getElementById("myImg").src = "light on.jpg";
+  document.getElementById("myImg").setAttribute("width", "200");
+  document.getElementById("myImg").setAttribute("height", "270");
+}
+
+
+//light off
+function lightOff() {
+  document.getElementById("myImg").src = "light off.jpg";
+  document.getElementById("myImg").setAttribute("width", "290");
+}
